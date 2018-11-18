68f6fa9f047ad79f4f91016c85ced702e28b7741
Alexander Held <0.alexheld@gmail.com>
2018-11-18 07:05:44 +0100

    uploading image for product

---
+++ GraniteHouse/Utitlity/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Utitlity
+{
+	public class SD
+	{
+		public const string DefaultProductsImage = "default_image.png";
+		public const string ImageFolder = @"images\ProductImage";
+	}
+}
